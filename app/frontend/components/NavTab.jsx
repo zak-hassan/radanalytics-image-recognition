c72306c26ceac9459954af1e3eef4384df6a0b34
Humair Khan <ehumairkhan@gmail.com>
2017-08-14 11:44:22 -0400

    Resolved conflict on the modified files.

--- app/frontend/components/NavTab.jsx
+++ app/frontend/components/NavTab.jsx
@@ -3,30 +3,29 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 class NavTab extends React.Component {
+
   static get propTypes() {
     return {
       to: PropTypes.string,
+      activeRoute: PropTypes.string,
       children: PropTypes.string,
+      setRoute: PropTypes.func
     }
   }
 
   render() {
-      let isActive = this.context.router.route.location.pathname === this.props.to;
-      let className = isActive ? "active" : "";
+      var isActive = this.props.activeRoute === this.props.to;
+      var className = isActive ? "active" : "";
 
       return (
-        <li className={className}>
-          <Link {...this.props}>
+        <li className={className} onClick={() => this.props.setRoute(this.props.to)}>
+          <Link to={this.props.to}>
               {this.props.children}
           </Link>
         </li>
       );
   }
 }
-
-NavTab.contextTypes = {
-    router: PropTypes.object
-};
 
 export default NavTab;
 
--- app/frontend/containers/App.jsx
+++ app/frontend/containers/App.jsx
@@ -14,37 +14,41 @@ import ConfigView from "./ConfigView.jsx";
 import StatsView from "./StatsView.jsx";
 
 import { clearMessage } from "../actions/messageActions";
+import { setRoute } from "../actions/navActions";
 
 class App extends Component {
+
   static get propTypes() {
     return {
       message: PropTypes.string,
       messageType: PropTypes.string,
       icon: PropTypes.string,
       visible: PropTypes.bool,
-      clearMessage: PropTypes.func
+      clearMessage: PropTypes.func,
+      setRoute: PropTypes.func,
+      activeRoute: PropTypes.string
     }
   }
 
   render() {
     return (
-      <Router>
-        <div className="app">
-          <Navbar/>
-          <Message message={this.props.message} messageType={this.props.messageType}
-                   icon={this.props.icon} visible={this.props.visible}
-                   clearMessage={this.props.clearMessage}/>
-          <Switch>
-            <Route exact path="/" component={ImageClassifierView}/>
-          </Switch>
-          <Switch>
-            <Route exact path="/stats" component={StatsView}/>
-          </Switch>
-          <Switch>
-            <Route exact path="/config" component={ConfigView}/>
-          </Switch>
-        </div>
-      </Router>
+        <Router>
+          <div className="app">
+            <Navbar setRoute={this.props.setRoute} activeRoute={this.props.activeRoute}/>
+            <Message message={this.props.message} messageType={this.props.messageType}
+              icon={this.props.icon} visible={this.props.visible}
+              clearMessage={this.props.clearMessage}/>
+            <Switch>
+               <Route exact path="/" component={ImageClassifierView}/>
+            </Switch>
+            <Switch>
+               <Route exact path="/stats" component={StatsView}/>
+            </Switch>
+            <Switch>
+               <Route exact path="/config" component={ConfigView}/>
+            </Switch>
+          </div>
+        </Router>
     );
   }
 }
@@ -54,7 +58,8 @@ const mapStateToProps = (state) => {
     message: state.messageReducer.message,
     messageType: state.messageReducer.messageType,
     icon: state.messageReducer.icon,
-    visible: state.messageReducer.visible
+    visible: state.messageReducer.visible,
+    activeRoute: state.navReducer.activeRoute
   }
 };
 
@@ -62,9 +67,11 @@ const mapDispatchToProps = (dispatch) => {
   return {
       clearMessage: () => {
           dispatch(clearMessage())
+      },
+      setRoute: (route) => {
+          dispatch(setRoute(route))
       }
   }
 };
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
