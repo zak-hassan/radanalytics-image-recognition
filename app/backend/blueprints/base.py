bdd90c01d88dd805eafe4135b30e4886dee37cf9
Zak Hassan <zak.hassan1010@gmail.com>
2018-07-08 15:41:04 -0400

    Adding MobileNet model v2

--- app/backend/blueprints/base.py
+++ app/backend/blueprints/base.py
@@ -1,13 +1,15 @@
+from __future__ import print_function
+from __future__ import print_function
 from flask import jsonify, request, Response, Blueprint, render_template, send_file
 from flask import current_app as app
-from app.backend.inception import inception
+from app.backend.inception import inception, mobile_net
+from werkzeug.utils import secure_filename
+from infinispan.remotecache import RemoteCache
+from timeit import default_timer as timer
 import os.path
 import os
-from werkzeug.utils import secure_filename
 import json
-from infinispan.remotecache import RemoteCache
 import datetime
-from timeit import default_timer as timer
 import glob
 
 try:
@@ -114,44 +116,71 @@ def get_images():
 
 @basepage.route("/api/v1/historyServer", methods=['GET'])
 def get_jdg_history():
-    hostname= os.getenv('JDG_HOSTNAME','0.0.0.0')
-    basepath=os.getenv('BASE_PATH','/opt/imagerecognize/')
+    hostname = os.getenv('JDG_HOSTNAME', '0.0.0.0')
+    basepath = os.getenv('BASE_PATH', '/opt/imagerecognize/')
     remote_cache = RemoteCache(host=hostname)
-    #os.chdir(basepath)
+
+    # os.chdir(basepath)
 
     files=glob.glob(".rad-img-recog/*")
-    mock_data={}
-    mock_data["results"]=[]
-    bulk_data= remote_cache.bulk_get()
+    mock_data = {}
+    mock_data["results"] = []
+    bulk_data = remote_cache.bulk_get()
     for f in files:
-        name=bulk_data[(f.split("/")[1])]
-        data =json.loads(name)
-        #print data
+        name = bulk_data[(f.split("/")[1])]
+        data = json.loads(name)
+
+        # print data
         mock_data["results"].append({
             "filename": f.split("/")[1],
             "classification": data["pred"][0][1],
             "percentage":data["pred"][0][0]
         })
-    mock_data['time_taken']=remote_cache.get("time_taken")
-    mock_data['last_transaction']=remote_cache.get("last_transaction")
+    mock_data['time_taken'] = remote_cache.get("time_taken")
+    mock_data['last_transaction'] = remote_cache.get("last_transaction")
 
-    if (len(files) <10):
-        mock_data["num_results"]=len(files)
-        mock_data["num_pages"]=1
+    if len(files) < 10:
+        mock_data["num_results"]= len(files)
+        mock_data["num_pages"] = 1
     else:
-        mock_data["num_results"]=len(files)
-        total_files=len(files)
-        num= total_files/10
-        if( total_files %10 != 0):
-            num= num+1
-        mock_data["num_pages"]= num
+        mock_data["num_results"] = len(files)
+        total_files = len(files)
+        num = total_files/10
+        if total_files % 10 != 0:
+            num = num+1
+        mock_data["num_pages"] = num
     print (mock_data)
     return Response(json.dumps(mock_data), status=200, mimetype="application/json")
 
+
 @basepage.route("/api/v1/images/<image_name>")
 def serve_image(image_name):
-    path= app.config['UPLOAD_FOLDER'] + "/"+ image_name;
-    return send_file( path, mimetype='image/jpeg' )
+    path = app.config['UPLOAD_FOLDER'] + "/" + image_name
+    return send_file(path, mimetype='image/jpeg')
+
+
+# Use Inception Model
+def inception_get_scores(img_path):
+    inception.data_dir = app.config['INCEPTION_MODEL']
+    inception.maybe_download()
+    model = inception.Inception()
+    pred = model.classify(image_path=img_path)
+    scores = model.get_scores(pred=pred, k=5)
+    model.close()
+
+    return scores
+
+
+# Use mobileNet
+def mobilenet_get_scores(img_path):
+    mobile_net.data_dir = app.config['MOBILE_NET']
+    mobile_net.maybe_download()
+    model = mobile_net.MobileNet()
+    pred = model.classify(image_path=img_path)
+    scores = model.get_scores(pred=pred, k=5)
+
+    return scores
+
 
 @basepage.route("/api/v1/imgrecognize", methods=['POST', 'GET'])
 def img_recognize():
@@ -163,38 +192,32 @@ def img_recognize():
     file = request.files['file']
 
     # TODO :
-    #if file and allowed_file(file.filename):
+    # if file and allowed_file(file.filename):
 
     filename = secure_filename(file.filename)
     img_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     file.save(img_path)
 
-    # Use Inception Model
-    inception.data_dir = app.config['INCEPTION_MODEL']
-    inception.maybe_download()
-    model = inception.Inception()
-    pred = model.classify(image_path=img_path)
-    scores = model.get_scores(pred=pred, k=5)
-    model.close()
+    # scores = inception_get_scores(img_path)
+    scores = mobilenet_get_scores(img_path)
 
-    #Getting environment variable or using default localhost
-    hostname=os.getenv('JDG_HOSTNAME','0.0.0.0')
+    # Getting environment variable or using default localhost
+    hostname = os.getenv('JDG_HOSTNAME', '0.0.0.0')
 
     remote_cache = RemoteCache(host=hostname)
     times = datetime.datetime.now().strftime("%B-%d-%Y-%I:%M%p")
-    key= filename
+    key = filename
     remote_cache.put("last_transaction", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    remote_cache.put(key, json.dumps({'pred':scores, 'timestamp': times , 'filename': filename}) )
-    print remote_cache.stats()
-    print "Key: %s" % key
+    remote_cache.put(key, json.dumps({'pred': scores, 'timestamp': times, 'filename': filename}))
+    print(remote_cache.stats())
+    print("Key: %s" % key)
     # Send data
     resp = {'pred': scores}
 
     end = timer()
-    total=end - start
+    total = end - start
 
-
-    remote_cache.put( "time_taken" , str(total) )
+    remote_cache.put("time_taken", str(total))
 
     return Response(
         json.dumps(resp),
